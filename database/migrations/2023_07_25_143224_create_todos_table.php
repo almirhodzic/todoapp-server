15753b5510d5f5bfea418a24ec2437c0a6d5a9f1
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-10-24 19:53:32 +0200

    Update todos table structure (sql)

--- database/migrations/2023_07_25_143224_create_todos_table.php
+++ database/migrations/2023_07_25_143224_create_todos_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->default(0);
-            $table->unsignedBigInteger('list_id')->default(0);
             $table->string('title');
             $table->text('description')->nullable();
             $table->boolean('done')->default(false);
